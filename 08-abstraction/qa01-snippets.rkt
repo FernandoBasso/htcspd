123f29cfa598babe6b10c4c8db5c9f4e38045a23
Fernando Basso <fernandobasso.br@gmail.com>
2017-08-31 19:15:57 -0300

    qa: just the scratch files I used to answer the qa...

---
+++ 08-abstraction/qa01-snippets.rkt
@@ -0,0 +1,72 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname dev-qa) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+;
+;; Suppose you want to design a function that consumes (listof Image)
+;; and rotates each image by 90 degrees.
+;;
+;; Which built-in abstract function(s) will you use to code the body?
+;
+
+(define (rotate-all loi)
+  (map rotate-90 loi))
+
+(define (rotate-90 i)
+  (rotate 90 i))
+
+;
+;; Suppose you want to design a function that consumes (listof String) and
+;; combines each string in the list to create a single string.
+;;
+;; Which built-in abstract function(s) will you use to code the body?
+;
+
+(define (combine-los los)
+  (foldr string-append "" los))
+
+;
+;; Suppose you want to design a function that consumes (listof String) produces
+;; a list of the short strings. Assume short here means less than 10 characters
+;; long.
+;;
+;; Which built-in abstract function(s) will you use to code the body?
+;
+
+(define (short-los los)
+  (filter short? los))
+
+(define (short? s)
+  (< (string-length s) 10))
+
+(short-los (list "Master Yoda" "Obiwan Kenobi"))
+(short-los (list "Master Yoda!" "Leia" "Luke"))
+
+;
+;; Suppose you want to design a function that consumes a natural number, n,
+;; and produces a list of even numbers up to n.
+;;
+;; Which built-in abstract function(s) will you use to code the body?
+;
+(define (evens-to n)
+  (filter even? (build-list n identity)))
+
+(evens-to 6)
+;; 0 2 4 6
+
+;
+;;Suppose you want to design a function that consumes (listof Number) and
+;;produces true if the list contains a negative number.
+;;
+;;Which built-in abstract function(s) will you use to code the body?
+;
+(define (contains-negative? lon)
+  (ormap negative? lon))
+
+(contains-negative? (list 3 9 -5 5))
+(contains-negative? (list -1 -3))
+(contains-negative? (list 1 3 9))
+;; #t
+;; #t
+;; #f
